1c124c343a0ed00a66287846f01a9fa3ba4a37ce
Pablo Lencinas <pslencinas@gmail.com>
2021-10-22 19:44:18 -0300

    Update main.py
    Agregamos /saludar. /cargarUsuario

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from flask import Flask
-
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def hola_mundo():
     return 'Hola mundo desde Flask'
+
+
+@app.route('/test')
+def test():
+    return 'Hola mundo desde Flask, ruta /test'
+
+@app.route('/saludar')
+def saludar():
+    param = request.args.get('q', 'no tengo parametros')
+    return 'Hola {}'.format(param)
+
+@app.route('/cargarUsuario/<name>/<int:id>')
+def cargar(name, id):
+    
+    return 'Hola {} con id: {}'.format(name, id)
 
 
 if __name__ == '__main__':
